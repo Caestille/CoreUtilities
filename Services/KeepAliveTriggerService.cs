d638dfe70bdae9ac285181a32976265f2189f192
Joseph Ward <jward@mercedesamgf1.com>
2022-08-05 14:43:53 +0100

    Add keepAliveTriggerService for searchboxes

---
+++ Services/KeepAliveTriggerService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace CoreUtilities.Services
+{
+	public class KeepAliveTriggerService
+	{
+		private bool hasKeepAliveBeenRefreshed;
+		private bool block;
+		bool run = true;
+
+		public KeepAliveTriggerService(Action callback, int refreshTimeMs)
+		{
+			Thread thread = new Thread(new ThreadStart(() =>
+			{
+				while (run)
+				{
+					if (!hasKeepAliveBeenRefreshed && !block)
+					{
+						callback();
+						block = true;
+					}
+
+					if (hasKeepAliveBeenRefreshed)
+					{
+						hasKeepAliveBeenRefreshed = false;
+						block = false;
+					}
+
+					Thread.Sleep(refreshTimeMs);
+				}
+			}));
+			thread.Start();
+		}
+
+		public void Refresh()
+		{
+			hasKeepAliveBeenRefreshed = true;
+		}
+
+		public void Stop()
+		{
+			run = false;
+		}
+	}
+}
