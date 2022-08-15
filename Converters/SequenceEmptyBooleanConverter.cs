a0406ed34a798c1c6317a60f57ad9dbe8daa8597
Joseph <jward2100@gmail.com>
2022-08-15 23:04:07 +0100

    Add new converter

---
+++ Converters/SequenceEmptyBooleanConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace CoreUtilities.Converters
+{
+	public class SequenceEmptyBooleanConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			IEnumerable<object> list = (IEnumerable<object>)value;
+			return !list.Any();
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return Binding.DoNothing;
+		}
+	}
+}
