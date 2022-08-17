d44ccb5979537c49128a22264f1408b716162e8f
Joseph <jward2100@gmail.com>
2022-08-17 11:49:36 +0100

    Add interface for dialogue service

---
+++ Interfaces/IDialogueService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CoreUtilities.Interfaces
+{
+	public interface IDialogueService
+	{
+		void RegisterViewForViewModel(Type viewType, Type vmType);
+
+		Task OpenCustomDialogue(object? dataContext = null);
+
+		void OpenFileDialogue();
+
+		void ShowMessageBox(string title, string message, MessageBoxButton button);
+	}
+}
