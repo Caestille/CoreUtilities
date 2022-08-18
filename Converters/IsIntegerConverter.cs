12a25d52a133a5a57c2797f670ee1350dfa4aba4
Joseph <jward2100@gmail.com>
2022-08-18 02:10:02 +0100

    Add new converter

---
+++ Converters/IsIntegerConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CoreUtilities.Converters
+{
+	public class IsIntegerConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return System.Convert.ToInt32(value) == int.Parse((string)parameter);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
