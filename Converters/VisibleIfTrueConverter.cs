aebc2dd4daa48ac36025b497c06add44b72d0dda
Joseph <jward2100@gmail.com>
2022-10-22 15:42:44 +0100

    Make visible if true/false converters more customisable

--- Converters/VisibleIfTrueConverter.cs
+++ Converters/VisibleIfTrueConverter.cs
@@ -7,18 +7,18 @@ namespace CoreUtilities.Converters
 {
 	/// <summary>
 	/// An <see cref="IValueConverter"/> which returns <see cref="Visibility.Visible"/> if the value provided in the
-	/// <see cref="Binding"/> is <see cref="false"/>, else <see cref="Visibility.Collapsed"/>.
+	/// <see cref="Binding"/> is <see cref="true"/>, else <see cref="Visibility.Collapsed"/>.
 	/// </summary>
-	public class VisibleIfFalseConverter : IValueConverter
+	public class VisibleIfTrueConverter : IValueConverter
 	{
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 		{
 			var visType = parameter != null
-				? ((string)parameter) == "Collapsed" ? Visibility.Collapsed : ((string)parameter) == "Hidden"
+				? ((string)parameter) == "Collapsed" ? Visibility.Collapsed : ((string)parameter) == "Hidden" 
 					? Visibility.Hidden
 					: Visibility.Collapsed
 				: Visibility.Collapsed;
-			return (bool)value ? visType : Visibility.Visible;
+			return (bool)value ? Visibility.Visible : visType;
 		}
 
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
