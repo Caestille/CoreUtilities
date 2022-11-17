298352b2a382a284e1e92ff7eb8b4b25b1480bdd
Joseph <jward2100@gmail.com>
2022-11-17 13:24:34 +0000

    Colour picker dialogue accepts input colour

--- Interfaces/Dialogues/IDialogueService.cs
+++ Interfaces/Dialogues/IDialogueService.cs
@@ -5,39 +5,40 @@ using System.Windows.Media;
 
 namespace CoreUtilities.Interfaces.Dialogues
 {
-    /// <summary>
-    /// Interface for classes managing dialogues to inherit.
-    /// </summary>
-    public interface IDialogueService
-    {
-        /// <summary>
-        /// Registers a view type to associate with a viewmodel type.
-        /// </summary>
-        /// <param name="viewType">The type of view to register.</param>
-        /// <param name="vmType">The type of viewmodel to register to.</param>
-        void RegisterViewForViewModel(Type viewType, Type vmType);
+	/// <summary>
+	/// Interface for classes managing dialogues to inherit.
+	/// </summary>
+	public interface IDialogueService
+	{
+		/// <summary>
+		/// Registers a view type to associate with a viewmodel type.
+		/// </summary>
+		/// <param name="viewType">The type of view to register.</param>
+		/// <param name="vmType">The type of viewmodel to register to.</param>
+		void RegisterViewForViewModel(Type viewType, Type vmType);
 
-        /// <summary>
-        /// Given a datacontext (viewmodel), opens a registered dialogue (if it exists)
-        /// </summary>
-        /// <param name="dataContext">An <see cref="object"/> which is the datacontext of the view to be opened
-        /// (if registered).</param>
-        /// <param name="dialogueSize">An overriding dialogue size if required, otherwise it is automatic (which
-        /// can have undesired results).</param>
-        /// <returns>An (awaitable) <see cref="Task"/>.</returns>
-        Task ShowCustomDialogue(object? dataContext = null, Size? dialogueSize = null);
+		/// <summary>
+		/// Given a datacontext (viewmodel), opens a registered dialogue (if it exists)
+		/// </summary>
+		/// <param name="dataContext">An <see cref="object"/> which is the datacontext of the view to be opened
+		/// (if registered).</param>
+		/// <param name="dialogueSize">An overriding dialogue size if required, otherwise it is automatic (which
+		/// can have undesired results).</param>
+		/// <returns>An (awaitable) <see cref="Task"/>.</returns>
+		Task ShowCustomDialogue(object? dataContext = null, Size? dialogueSize = null);
 
-        /// <summary>
-        /// Opens an open file dialogue.
-        /// </summary>
-        /// <returns>The path to open.</returns>
-        string ShowOpenFileDialogue();
+		/// <summary>
+		/// Opens an open file dialogue.
+		/// </summary>
+		/// <returns>The path to open.</returns>
+		string ShowOpenFileDialogue();
 
 		/// <summary>
 		/// Opens a colour picker dialogue.
 		/// </summary>
+		/// <param name="inputColour">The startying colour of the dialogue.</param>
 		/// <returns>The picked <see cref="Color"/>.</returns>
-		Color ShowColourPickerDialogue();
+		Color ShowColourPickerDialogue(Color inputColour);
 
 		/// <summary>
 		/// Shows a message box to display to the user.
@@ -46,5 +47,5 @@ namespace CoreUtilities.Interfaces.Dialogues
 		/// <param name="message">The message in the message box.</param>
 		/// <param name="button">The type of confirmation button(s) the message box should display.</param>
 		void ShowMessageBox(string title, string message, MessageBoxButton button);
-    }
+	}
 }
