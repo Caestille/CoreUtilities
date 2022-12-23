ca9e0314abdd2a4f8628841644d7461e2f1231f6
Joseph <jward2100@gmail.com>
2022-12-23 12:25:04 +0000

    Borderless window option for dialogue view

--- Interfaces/Dialogues/IDialogueService.cs
+++ Interfaces/Dialogues/IDialogueService.cs
@@ -18,7 +18,7 @@ namespace CoreUtilities.Interfaces.Dialogues
 		void RegisterViewForViewModel(Type viewType, Type vmType);
 
 		/// <summary>
-		/// Given a datacontext (viewmodel), opens a registered dialogue (if it exists)
+		/// Given a datacontext (viewmodel), opens a registered dialogue (if it exists) in a standard window.
 		/// </summary>
 		/// <param name="dataContext">An <see cref="object"/> which is the datacontext of the view to be opened
 		/// (if registered).</param>
@@ -26,6 +26,16 @@ namespace CoreUtilities.Interfaces.Dialogues
 		/// can have undesired results).</param>
 		/// <returns>An (awaitable) <see cref="Task"/>.</returns>
 		Task ShowCustomDialogue(object? dataContext = null, Size? dialogueSize = null);
+
+		/// <summary>
+		/// Given a datacontext (viewmodel), opens a registered dialogue (if it exists) in a borderless window.
+		/// </summary>
+		/// <param name="dataContext">An <see cref="object"/> which is the datacontext of the view to be opened
+		/// (if registered).</param>
+		/// <param name="dialogueSize">An overriding dialogue size if required, otherwise it is automatic (which
+		/// can have undesired results).</param>
+		/// <returns>An (awaitable) <see cref="Task"/>.</returns>
+		Task ShowBorderlessCustomDialogue(object? dataContext = null, Size? dialogueSize = null);
 
 		/// <summary>
 		/// Opens an open file dialogue.
