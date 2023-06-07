165f1090c327a6c78d2b7d43d6a54d0770cc7955
Joseph <jward2100@gmail.com>
2023-06-07 01:18:39 +0100

    Add visible if null converter

---
+++ Converters/VisibleIfNullConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CoreUtilities.Converters
+{
+	/// <summary>
+	/// An <see cref="IValueConverter"/> which returns <see cref="Visibility.Visible"/> if the value provided in the 
+	/// <see cref="Binding"/> is <see cref="null"/>, else <see cref="Visibility.Collapsed" />.
+	/// </summary>
+	public class VisibleIfNullConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return (value != null) ? Visibility.Collapsed : Visibility.Visible;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
