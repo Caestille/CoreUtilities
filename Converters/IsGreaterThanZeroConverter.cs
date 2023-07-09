9b18606bf9dea2ba4e97d25f48746d96e65561f1
Joseph <jward2100@gmail.com>
2023-07-10 00:23:41 +0100

    New converter

---
+++ Converters/IsGreaterThanZeroConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CoreUtilities.Converters
+{
+	/// <summary>
+	/// A <see cref="IValueConverter"/> which returns <see cref="true"/> if the given value is > 0.
+	/// </summary>
+	public class IsGreaterThanZeroConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return (double)value > 0;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return Binding.DoNothing;
+		}
+	}
+}
