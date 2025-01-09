d5187cb21b78f7979006491881895da35e690887
Joseph <jward2100@gmail.com>
2025-01-09 16:44:23 +0000

    Use community toolkit

--- HelperClasses/EvaluationRules/BaseRule.cs
+++ HelperClasses/EvaluationRules/BaseRule.cs
@@ -1,6 +1,6 @@
 ﻿using CoreUtilities.HelperClasses.Enums;
 using CoreUtilities.Interfaces.EvaluationRules;
-using Microsoft.Toolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
 
